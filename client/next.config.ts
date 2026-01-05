70e68efc2bed6cb9f12011a4a381648851ff7d0b
SakthiMuthuselvan <sakthimsd531@gmail.com>
2026-01-05 22:58:18 +0530

    Changes

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.pexels.com", "images.unsplash.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
